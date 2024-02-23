8298f9e6ba4b679f57c3dff71b38fa25f37db76d
chintu413 <59384502+chintu413@users.noreply.github.com>
2024-02-23 13:23:25 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -10,3 +10,7 @@ def tomcatDeploy(jobname,ip,context)
 {
   sh"scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war abhi@${ip}:/var/lib/tomcat9/webapps/${context}.war"
 }
+def excuteselinium(jobname)
+{
+  sh "java -jar /var/lib/jenkins/workspace/${jobname}/webapp/target/testing.war"
+}
